16317368874a8211024af32d9806bbadafa139c6
Irityan <thinkabout42@yandex.ru>
2020-03-23 16:11:32 +0300

    Initial documentation

--- js/editor-common.js
+++
@@ -1,9 +0,0 @@
-function exitWarning(returnPage) {
-    confirm("Выход", "Вы действительно хотите выйти без сохранения?").then(function(answer) {
-        if (answer) {
-            window.location = returnPage;
-        } else {
-            //console.log("Выход отменён");
-        }
-    });
-}
